ff612b0aeeeda827b9133699bfc43838d4670036
ccBond <1315275049@qq.com>
2023-09-08 15:01:28 +0800

    feat: add heterogenous data loader

---
+++ data_loader/hete_graph_data.py
@@ -0,0 +1,41 @@
+import os
+import torch
+import logging
+
+
+def load_hete_graph_data(dataset_name, device, data_dir='./data'):
+    """
+    Load the heterogeneous graph data.
+
+    Parameters:
+    - dataset_name (str): Name of the dataset.
+    - device (torch.device): Device to store the data.
+    - data_dir (str): Path to the directory where the data is stored.
+
+    Returns:
+    - HeteroData: The heterogeneous graph data.
+    - int: Number of classes.
+    - Tensor: Training mask.
+    - Tensor: Validation mask.
+    - Tensor: Test mask.
+    - Tensor: Labels.
+    """
+
+    path = os.path.abspath(os.path.dirname(os.getcwd()) + data_dir)
+
+    if dataset_name == 'DBLP':
+        from torch_geometric.datasets import DBLP
+
+        dataset = DBLP(path)
+        graph = dataset[0]
+        num_classes = torch.max(graph['author'].y).item() + 1
+        graph['conference'].x = torch.ones((graph['conference'].num_nodes, 1))
+        graph = graph.to(device)
+        train_mask, val_mask, test_mask = graph['author'].train_mask, graph[
+            'author'].val_mask, graph['author'].test_mask
+        y = graph['author'].y
+
+        logging.info("Loaded DBLP dataset. Number of nodes: %d, number of edges: %d, number of classes: %d" % (
+            graph.num_nodes, graph.num_edges, num_classes))
+
+        return graph, num_classes, train_mask, val_mask, test_mask, y
