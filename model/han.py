e8d6dbdd6f7ab02ee72f7697c97f0e5a1eee6204
ccBond <1315275049@qq.com>
2023-09-12 14:40:09 +0800

    feat: add han in model

---
+++ model/han.py
@@ -0,0 +1,41 @@
+from torch import nn
+from torch_geometric.nn import HANConv
+
+
+class HAN(nn.Module):
+    """
+    Heterogeneous Graph Attention Network (HAN) model.
+    """
+
+    def __init__(self, in_channels, hidden_channels, out_channels, graph):
+        """
+        Initialize the HAN model.
+
+        Parameters:
+        - in_channels (int): Number of input features.
+        - hidden_channels (int): Number of hidden units.
+        - out_channels (int): Number of output features.
+        - graph (HeteroData): Input graph.
+        """
+        super(HAN, self).__init__()
+        self.conv1 = HANConv(in_channels, hidden_channels,
+                             graph.metadata(), heads=4)
+        self.conv2 = HANConv(hidden_channels, out_channels,
+                             graph.metadata(), heads=4)
+
+    def forward(self, data):
+        """
+        Forward pass for the HAN model.
+
+        Parameters:
+        - data (HeteroData): Input data.
+
+        Returns:
+        - Tensor: Output data.
+        """
+        x_dict, edge_index_dict = data.x_dict, data.edge_index_dict
+        x = self.conv1(x_dict, edge_index_dict)
+        x = self.conv2(x, edge_index_dict)
+        x = x['author']
+
+        return x
