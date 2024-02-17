394cbb77fdbd1360d0a44735e43467e987901c3f
ccbond <1315275049@qq.com>
2024-02-18 01:13:04 +0800

    feat: train han in different datasets

--- han.py
+++ han.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import Any, List
 
 import torch
 import torch.nn.functional as F
@@ -14,19 +14,40 @@ torch.cuda.empty_cache()
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+def train(model, data, optimizer, target_node_type) -> float:
+    model.train()
+
+    optimizer.zero_grad()
+    data = data.to(device)
+    out = model(data.x_dict, data.edge_index_dict, target_node_type)
+    mask = data[target_node_type].train_mask
+    loss = F.cross_entropy(out[mask], data[target_node_type].y[mask])
+    loss.backward()
+    optimizer.step()
+    
+    return loss.item()
+
+
+@torch.no_grad()
+def test(model, data, target_node_type) -> List[float]:
+    model.eval()
+
+    pred = model(data.x_dict, data.edge_index_dict, target_node_type).argmax(dim=-1)
+
+    accs = []
+    for split in ['train_mask', 'val_mask', 'test_mask']:
+        mask = data[target_node_type][split].to(device)
+        acc = ((pred[mask] == data[target_node_type].y[mask]).sum() / mask.sum())
+        accs.append(acc.item())
+    
+    return accs
+
+
 for dataset in all_datasets:
     logging.info(f"Loading dataset: {dataset}")
     data = load_full_dataset(dataset, True, True)
     target_node_type = get_data_target_node_type(dataset)
     
-    # # create train_loader
-    # train_idx = data[target_node_type].train_mask.nonzero(as_tuple=True)[0]
-    # train_loader = NeighborLoader(data, num_neighbors=[15, 15], input_nodes=(target_node_type, train_idx), input_time=None, replace=False, subgraph_type="directional", disjoint=False, temporal_strategy="uniform", time_attr=None, weight_attr=None, transform=None, transform_sampler_output=None, is_sorted=False, filter_per_worker=None,neighbor_sampler=None, batch_size=128,directed=True,shuffle=True)
-
-    # # create test_loader
-    # test_idx = data[target_node_type].test_mask.nonzero(as_tuple=True)[0]
-    # test_loader = NeighborLoader(data, num_neighbors=[15, 15], input_nodes=(target_node_type, test_idx), input_time=None, replace=False, subgraph_type="directional", disjoint=False, temporal_strategy="uniform", time_attr=None, weight_attr=None, transform=None, transform_sampler_output=None, is_sorted=False, filter_per_worker=None,neighbor_sampler=None, batch_size=128,directed=True,shuffle=True)
-
     num_classes = data[target_node_type].y.max().item() + 1
 
     model = init_model('han', num_classes, data)
@@ -37,50 +58,11 @@ for dataset in all_datasets:
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
 
-
-    def train() -> float:
-        model.train()
-
-        optimizer.zero_grad()
-        data = data.to(device)
-        batch_size = data[target_node_type].batch_size
-        out = model(data.x_dict, data.edge_index_dict, target_node_type)
-
-        loss = F.cross_entropy(out[target_node_type], data[target_node_type].y)
-        loss.backward()
-        optimizer.step()
-        
-        return loss.item()
-
-
-    @torch.no_grad()
-    def test() -> List[float]:
-        model.eval()
-        
-        data = data.to(device)
-        out = model(data.x_dict, data.edge_index_dict, target_node_type).argmax(dim=-1)
-        
-
-        # 初始化正确统计和总数统计
-        correct = {split: 0 for split in ['train', 'val', 'test']}
-        total = {split: 0 for split in ['train', 'val', 'test']}
-
-        for split in ['train_mask', 'val_mask', 'test_mask']:
-            mask = data['author'][split].to(device)
-            total[split] += mask.sum().item()
-            correct[split] += (out[mask] == data['author'].y[mask]).sum().item()
-
-        # 计算每个分割的准确率
-        accs = [correct[split] / total[split] for split in ['train', 'val', 'test']]
-        
-        return accs
-
     best_val_acc = 0
     start_patience = patience = 200
     for epoch in range(1, 200):
-
-        loss = train()
-        train_acc, val_acc, test_acc = test()
+        loss = train(model, data, optimizer, target_node_type)
+        train_acc, val_acc, test_acc = test(model, data, target_node_type)
         print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
             f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
 
